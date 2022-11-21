6abdb2fd54434e320bc60b8f4a8b8fcda1f733e4
Kai Kim <juhyeon1114@gmail.com>
2022-11-21 19:52:49 +0900

    log : init

---
+++ src/main/java/hello/springbasicitems/SpringBasicItemsApplication.java
@@ -0,0 +1,13 @@
+package hello.springbasicitems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBasicItemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBasicItemsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/springbasicitems/SpringBasicItemsApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.springbasicitems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBasicItemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
